986cf4ed8d48938baf695de03dbdc6ef8d8ec6e4
katyadonovan <you@example.com>
2017-11-13 09:53:56 -0500

    adding noise

--- code/noise.py
+++ code/noise.py
@@ -1,24 +1,27 @@
 import random as random
 class Noise(r,q):
     """ Noise class """
-    def __init__(self,q):
+    def __init__(self,loc, q):
         self.q = q
         self.r = r
         self.decisions = []
         self.agents = Agents.agents()
         self.empty_locs = PrisonerDilemma.empty_locs()
-        self.loc = PrisonerDilemma.loc()
+        self.loc = loc
 
-    def choosing_strategy(self):
+    def changing_strategy(self):
+        "strategy changes from imitation to randomly choosing to cooperate or defect"
         if self.q >= random.randomint(0,1):
             self.decisions.append('C')
         else:
             self.decisions.append('D')
 
-    def move_randomly(self):
+    def migrate_randomly(self):
+        "migration is completely random"
         self.loc = self.empty_locs[random.randint(0,len(empty_locs)-1)]
         return self.loc
 
 
     def both_noise(self):
-        return self.choosing_strategy(), self.move_randomly()
+        "agent will migrate randomly and choose their strategy with a biased random"
+        return self.changing_strategy(), self.migrate_randomly()
