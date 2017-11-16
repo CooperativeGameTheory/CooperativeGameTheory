32b604a89fdfdb8d2bdb6672e3df39b2b7ad3bb6
katyadonovan <you@example.com>
2017-11-16 09:39:32 -0500

    fixing noise

--- code/noise.py
+++ code/noise.py
@@ -1,27 +1,45 @@
 import random as random
-class Noise(r,q):
+class Noise(Environment):
     """ Noise class """
-    def __init__(self,loc, q):
-        self.q = q
+    def __init__(self,loc,q=.05,r=.05):
+        super(Environment, self).__init__( L=49, seed=0, R=10, S=0, T=13, P=1, **config)
+        self.q = q #will choose to cooperate with probabiility 1-q
         self.r = r
-        self.decisions = []
-        self.agents = Agents.agents()
-        self.empty_locs = PrisonerDilemma.empty_locs()
-        self.loc = loc
 
     def changing_strategy(self):
         "strategy changes from imitation to randomly choosing to cooperate or defect"
-        if self.q >= random.randomint(0,1):
-            self.decisions.append('C')
-        else:
-            self.decisions.append('D')
+        "noah implemented this in his class"
+        pass
 
-    def migrate_randomly(self):
-        "migration is completely random"
-        self.loc = self.empty_locs[random.randint(0,len(empty_locs)-1)]
-        return self.loc
+    def migrate(self):
+        "migration is completely random - taking Tony's code because this is random migration"
+        n, _ = self.size
+        env = self.env
+        agents = self.agents
 
+        # Probability of random relocation
+        p = .05
 
-    def both_noise(self):
-        "agent will migrate randomly and choose their strategy with a biased random"
-        return self.changing_strategy(), self.migrate_randomly()
+        # Choose migrating agents
+        migrator_indices = np.argwhere(self.env > 0)
+        self._seed()
+        r = np.random.rand(migrator_indices.shape[0])
+        migrator_indices = migrator_indices[r<p]
+        if len(migrator_indices):
+            self._seed()
+            np.random.shuffle(migrator_indices)
+
+        # numpy array of empty indices of shape (k, 2)
+        empty_indices = np.argwhere(self.env == 0)
+
+        # for each migrating agent, choose random destination
+        for source in migrator_indices:
+            source = tuple(source)
+            self._seed()
+            i = np.random.randint(len(empty_indices))
+            dest = tuple(empty_indices[i])
+
+            # move
+            agents[dest] = agents[source]
+            agents[source] = None
+            empty_indices[i] = list(source)
